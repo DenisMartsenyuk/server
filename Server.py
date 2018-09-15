65890dfd175524fc1f8518d2e078c0e6b4bfa6fb
DenisMartsenyuk <35797844+DenisMartsenyuk@users.noreply.github.com>
2018-09-15 15:18:31 +0300

    Second_version

--- Server.py
+++ Server.py
@@ -1,22 +1,41 @@
 import socket
 
-HOST = "127.0.0.1"
-PORT = 60001
+PORT = 1337
 
 serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
-serv_sock.bind(('', 60001))
+serv_sock.bind(('', PORT))
 serv_sock.listen(2)
 
 while True:
     print("Start listen")
 
-    client_sock1, client_addr1 = serv_sock.accept()
+    client_sock0, client_addr1 = serv_sock.accept()
     print("I connect first player")
-    client_sock1.sendall(b'0')
+    client_sock0.sendall(b'0')
 
-    client_sock2, client_addr2 = serv_sock.accept()
+    client_sock1, client_addr2 = serv_sock.accept()
     print("I connect second player")
-    client_sock2.sendall(b'1')
+    client_sock1.sendall(b'1')
 
+    player = 0
+
+    while True:
+        if not player:
+            data = client_sock0.recv(1024)
+            if not data:
+                print("Player 1 RIP")
+                client_sock1.sendall(b'Connection end')
+                break
+            client_sock1.sendall(data)
+        else:
+            data = client_sock1.recv(1024)
+            if not data:
+                print("Player 2 RIP")
+                client_sock0.sendall(b'Connection end')
+                break
+            client_sock0.sendall(data)
+        player += 1
+        player %= 2
+
+    client_sock0.close()
     client_sock1.close()
-    client_sock2.close()
